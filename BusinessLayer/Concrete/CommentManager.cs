15325e5ee5f5f5aa77a65bfc431b8d3fb7232cfc
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2022-01-12 22:46:30 +0300

    29 - Yorumlara Ait Metotların Yazılması

---
+++ BusinessLayer/Concrete/CommentManager.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CommentManager : ICommentService
+    {
+        ICommentDAL _commentDAL;
+
+        public CommentManager(ICommentDAL commentDAL)
+        {
+            _commentDAL = commentDAL;
+        }
+
+        public void CommentAdd(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comment> GetList(int id)
+        {
+            return _commentDAL.GetListAll(x => x.BlogID == id);
+        }
+    }
+}
